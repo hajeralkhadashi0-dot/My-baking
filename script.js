520fea2d084e2a6df89118f25363caff2cd5f016
hajeralkhadashi0-dot <hajeralkhadashi0@gmail.com>
2026-04-13 07:05:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Baking website loaded");
+
+window.alert("Welcome to my baking website! 🍰");
+
+function showMessage() {
+    alert("Hope you enjoyed my sweet baking world! 🍰");
+    console.log("User clicked the baking button");
+} 
